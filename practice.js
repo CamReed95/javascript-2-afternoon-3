d19d4cc60600d58caa8622ad970aaa69d97c75e3
Cameron Reed <cameronreedadjusting@gmail.com>
2020-02-26 18:24:30 -0700

    Complete

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(arr, cb){
+  cb(arr[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,10 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(arr, cb){
+cb(arr.pop(arr.length -1))
+}
+
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +73,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, cb){
+  cb(num1*num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +95,16 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(arr, name, cb){
+for (i = 0; i < arr.length; i++){
+if (arr[i] === name){
+  cb(true)
+} else { 
+  cb(false)
+}
+}
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +126,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(arr, cb){
+  let newArr = []
+  for (i = 0; i < arr.length; i++){
+    if (newArr.indexOf(arr[i]) === -1){
+      newArr.push(arr[i])
+    }
+  }
+  cb(newArr)
+}
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +153,12 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(arr, cb){
+for (let i = 0; i < arr.length; i++){
+  cb(arr[i], i)
+}
+}
+
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,6 +176,13 @@ each(names, function(item, indice){
 */
 
 // Code here
+function getUserById(users, id, cb){
+for (let i = 0; i < users.length; i++){
+  if (users[i].id === id){
+    cb(users[i])
+  }
+}
+}
 
 // Do not edit the code below.
 var users = [
